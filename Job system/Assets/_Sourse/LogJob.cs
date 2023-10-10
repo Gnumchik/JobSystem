e6e179fbfddfea7be45ac3a2dc1d506f564c1684
danila149 <74356612+danila149@users.noreply.github.com>
2023-10-10 21:03:11 +0300

    GG

---
+++ Job system/Assets/_Sourse/LogJob.cs
@@ -0,0 +1,22 @@
+using Unity.Jobs;
+using UnityEngine;
+
+
+public struct LogJob : IJob
+{
+    public int Num;
+    public int Delay;
+    public float DeltaTime;
+    public float Timer;
+
+    public void Execute()
+    {
+        if (Timer <= 0)
+        {
+            Debug.Log(Mathf.Log(Num));
+            Timer = Delay;
+        }
+        else
+            Timer -= DeltaTime;
+    }
+}
